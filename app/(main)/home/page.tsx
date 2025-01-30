098b369c980aeb7f483508e0bc42f588938578f6
Liben Hailu <libenhailu04@gmail.com>
2025-01-30 12:58:31 +0300

    feat: add home page

---
+++ app/(main)/home/page.tsx
@@ -0,0 +1,75 @@
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader } from '@/components/ui/card';
+import { CourseList } from '@/features/quiz';
+import {
+  AuthGetCurrentUserServer,
+  cookiesClient,
+} from '@/utils/amplify-utils';
+
+export default async function Home() {
+  const user = await AuthGetCurrentUserServer();
+  const { data: activeCourse } =
+    await cookiesClient.models.ActiveCourse.listActiveCourseByUserEmail(
+      {
+        userEmail: user?.signInDetails?.loginId,
+      }
+    );
+
+  const { data: courseData } =
+    await activeCourse[0].course();
+
+  const { data: courses } =
+    await cookiesClient.models.Course.list({
+      selectionSet: ['id', 'title'],
+      limit: 4,
+    });
+
+  return (
+    <div className="flex w-full flex-col gap-y-6">
+      <h1 className="text-lg font-medium">
+        Welcome to CodeIQ
+      </h1>
+      <Card>
+        <CardHeader>
+          <div className="flex justify-between items-center flex-wrap space-y-4">
+            <div>
+              <h3 className="font-medium">
+                {courseData
+                  ? courseData.title
+                  : 'Select a topic to practice'}
+              </h3>
+              <p className="text-sm">
+                {courseData
+                  ? 'Continue studying to enhance your skills.'
+                  : 'Choose a subject to enhance your skills.'}
+              </p>
+            </div>
+            <Button asChild>
+              {courseData ? (
+                <Link href={`quizzes/${courseData.id}`}>
+                  Continue
+                </Link>
+              ) : (
+                <Link href={`quizzes`}>Start</Link>
+              )}
+            </Button>
+          </div>
+        </CardHeader>
+      </Card>
+      <div className="flex justify-between flex-wrap items-center">
+        <h2 className="text-lg font-medium">
+          Topic preview
+        </h2>
+        <Button variant="outline">
+          View all
+          <ArrowRight />
+        </Button>
+      </div>
+
+      <CourseList courses={courses} />
+    </div>
+  );
+}
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,6 +11,10 @@ export default function MainLayout({
       <div className="pt-16 flex-1 mx-auto px-4 w-full max-w-[1308px]">
         {children}
       </div>
+      <footer className="py-4 text-center">
+        <p>&copy; 2025 CodeIq. All rights reserved.</p>
+        <div className="social-links mt-2"></div>
+      </footer>
     </div>
   );
 }
