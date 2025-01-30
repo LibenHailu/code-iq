9e9ed585590bdbe54873f8c60270367c37ae7d3b
Liben Hailu <libenhailu04@gmail.com>
2025-01-30 16:14:18 +0300

    feat: implement landing page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,55 +1,41 @@
-'use client';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { Amplify } from 'aws-amplify';
-import { generateClient } from 'aws-amplify/data';
-import { useState, useEffect } from 'react';
-
-import type { Schema } from '@/amplify/data/resource';
-import outputs from '@/amplify_outputs.json';
-// import '@aws-amplify/ui-react/styles.css';
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setTodos] = useState<
-    Array<Schema['Todo']['type']>
-  >([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt('Todo content'),
-    });
-  }
-
+import { Button } from '@/components/ui/button';
+import { AuthGetCurrentUserServer } from '@/utils/amplify-utils';
+export default async function LandingPage() {
+  const user = await AuthGetCurrentUserServer();
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new
-        todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+    <div className="max-w-[988px] mx-auto shrink-0 flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero" />
       </div>
-    </main>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold max-w-[480px] text-center">
+          Learn, practice, and master new skills with
+          CodeIQ quizzes.
+        </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          {!user ? (
+            <Button
+              size="lg"
+              variant="outline"
+              className="w-full"
+            >
+              <Link href="/login">Get Started</Link>
+            </Button>
+          ) : (
+            <Button
+              size="lg"
+              variant="outline"
+              className="w-full"
+              asChild
+            >
+              <Link href="/home">Continue Learning</Link>
+            </Button>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
