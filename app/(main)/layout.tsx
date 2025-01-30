0ed4b1befec00113646d2e0afa338e69ab4f381a
Liben Hailu <libenhailu04@gmail.com>
2025-01-30 15:30:43 +0300

    fix: extract footer to its own component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/components/footer';
 import { Navbar } from '@/components/navbar';
 
 export default function MainLayout({
@@ -8,16 +9,10 @@ export default function MainLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="pt-16 flex-1 mx-auto px-4 h-full w-full max-w-[1308px]">
+      <div className="py-16 flex-1 mx-auto px-4 h-full w-full max-w-[1308px]">
         {children}
       </div>
-      <footer className="fixed z-50 bottom-0 px-4 w-full border-t shadow-sm  backdrop-filter backdrop-blur-lg">
-        <div className="max-w-[1308px] flex h-14 items-center justify-center mx-auto">
-          <p className="font-medium text-sm text-muted-foreground">
-            &copy; 2025 CodeIQ. All rights reserved.
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="z-50 px-4 w-full border-t shadow-sm  backdrop-filter backdrop-blur-lg">
+      <div className="max-w-[1308px] flex h-14 items-center justify-center mx-auto">
+        <p className="font-medium text-sm text-primary/80">
+          &copy; 2025 CodeIQ. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
