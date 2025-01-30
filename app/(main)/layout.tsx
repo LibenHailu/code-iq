660de0d619d084c0c59da5835d3e824d0230e43c
Liben Hailu <libenhailu04@gmail.com>
2025-01-30 13:28:52 +0300

    fix: layout loading component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,12 +8,15 @@ export default function MainLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="pt-16 flex-1 mx-auto px-4 w-full max-w-[1308px]">
+      <div className="pt-16 flex-1 mx-auto px-4 h-full w-full max-w-[1308px]">
         {children}
       </div>
-      <footer className="py-4 text-center">
-        <p>&copy; 2025 CodeIq. All rights reserved.</p>
-        <div className="social-links mt-2"></div>
+      <footer className="fixed z-50 bottom-0 px-4 w-full border-t shadow-sm  backdrop-filter backdrop-blur-lg">
+        <div className="max-w-[1308px] flex h-14 items-center justify-center mx-auto">
+          <p className="font-medium text-sm text-muted-foreground">
+            &copy; 2025 CodeIQ. All rights reserved.
+          </p>
+        </div>
       </footer>
     </div>
   );
--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -2,8 +2,8 @@ import { Loader2 } from 'lucide-react';
 
 const Loading = () => {
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      <Loader2 className="h-6 w-6 animate-spin " />
+    <div className="flex h-[80vh] items-center justify-center">
+      <Loader2 className="h-6 w-6 animate-spin" />
     </div>
   );
 };
